a592857f38d37f482fd09b47068d074eba555876
ValentinaPanic <valentina.panic87@gmail.com>
2020-12-16 23:39:33 -0700

    corrected references in ingrd table

--- db/migrate/20201217061547_create_ingredients.rb
+++ db/migrate/20201217061547_create_ingredients.rb
@@ -2,7 +2,7 @@ class CreateIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.cocktail :references
+      t.references :cocktail
 
       t.timestamps
     end
