fcedfd7c2af138eff7551af039fed566bc89dce1
ValentinaPanic <valentina.panic87@gmail.com>
2021-01-08 23:41:28 -0700

    cleaning unnecessary code

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,19 +1,12 @@
 class CocktailsController < ApplicationController
-  # before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
-
-  
+ 
   def index
     cocktails = Cocktail.all
 
     render json: CocktailSerializer.new(cocktails)
   end
 
-  def show
-  end
-
-  def edit
-  end
-
+  
   def create
       cocktail = Cocktail.new(cocktail_params)
       if cocktail.save
@@ -25,22 +18,20 @@ class CocktailsController < ApplicationController
       end
   end
 
-  # PATCH/PUT /cocktails/1
-  # PATCH/PUT /cocktails/1.json
-  def update
-    respond_to do |format|
-      if cocktail.update(cocktail_params)
-        format.html { redirect_to cocktail, notice: 'Cocktail was successfully updated.' }
-        format.json { render :show, status: :ok, location: cocktail }
-      else
-        format.html { render :edit }
-        format.json { render json: cocktail.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+ 
+  # def update
+  #   respond_to do |format|
+  #     if cocktail.update(cocktail_params)
+  #       format.html { redirect_to cocktail, notice: 'Cocktail was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: cocktail }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: cocktail.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /cocktails/1
-  # DELETE /cocktails/1.json
+  
   def destroy
     cocktail = Cocktail.find(params[:id])
     cocktail.destroy
@@ -48,12 +39,7 @@ class CocktailsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cocktail
-      cocktail = Cocktail.find(params[:id])
-    end
-
- 
+   
     def cocktail_params
       params.require(:cocktail).permit(:name, :image, :instructions)
     end
--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,20 +1,9 @@
 class IngredientsController < ApplicationController
-  # before_action :set_ingredient, only: [:show, :edit, :update, :destroy]
-
-
+ 
   def index
     ingredients = Ingredient.all
     render json: ingredients
-    # render json: IngredientSerializer.new(ingredients)
-  end
-
-  # GET /ingredients/1
-  # GET /ingredients/1.json
-  def show
-  end
-
-  # GET /ingredients/1/edit
-  def edit
+  
   end
 
   def create
@@ -23,29 +12,25 @@ class IngredientsController < ApplicationController
   
       if ingredient.save
        render json: ingredient
-        #  render json: IngredientSerializer.new(ingredient), status: :accepted
+      
       else
- 
         render json: {errors: ingredient.errors.full_messages}, status: :unprocessable_entity 
     end
   end
 
-  # PATCH/PUT /ingredients/1
-  # PATCH/PUT /ingredients/1.json
-  def update
-    respond_to do |format|
-      if @ingredient.update(ingredient_params)
-        format.html { redirect_to @ingredient, notice: 'Ingredient was successfully updated.' }
-        format.json { render :show, status: :ok, location: @ingredient }
-      else
-        format.html { render :edit }
-        format.json { render json: @ingredient.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  
+  # def update
+  #   respond_to do |format|
+  #     if @ingredient.update(ingredient_params)
+  #       format.html { redirect_to @ingredient, notice: 'Ingredient was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @ingredient }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @ingredient.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /ingredients/1
-  # DELETE /ingredients/1.json
   def destroy
     ingredient = Ingredient.find(params[:id])
    ingredient.destroy
